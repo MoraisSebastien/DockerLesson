aea944ebaf887c8665e322837d61171ce186f251
Sebastien Morais <sebastien.morais@cea.fr>
2021-11-27 14:56:56 +0100

    LAMP: Sort Articles By Their Id

--- stack_lamp/app/index.php
+++ stack_lamp/app/index.php
@@ -5,16 +5,16 @@ error_reporting(E_ALL);
 
 require 'db-config.php';
 
+function getArticles(PDO $PDO)
+{
+  $sql = "SELECT * FROM articles ORDER BY id DESC";
+  $result = $PDO->query($sql);
 
-function getArticles(PDO $PDO){
-    $sql = "SELECT * FROM articles";
-    $result = $PDO->query($sql);
+  $articles = $result->fetchAll(PDO::FETCH_ASSOC);
 
-    $articles = $result->fetchAll(PDO::FETCH_ASSOC);
+  $result->closeCursor();
 
-    $result->closeCursor();
-
-    return $articles;
+  return $articles;
 }
 ?>
 
